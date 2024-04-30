94beab75c94433bea9054c99c88ec06bd71b22a7
molarovega <molarofrancisco@hotmail.com>
2024-04-30 19:43:29 -0300

    cree el ejercicio nuevo

---
+++ MiSolucion/Proyecto1/Program.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace MiProyecto1
+{
+   public class Program
+   {
+      static public void Main(string [] args)
+      {
+        //PROCESO PROYECTO1 
+          
+          //ESCRIBIR 'Hola mundo'
+          Console.WriteLine("Hola mundo");        
+          
+
+
+
+        //FINPROCESO
+      }   
+   }
+}
